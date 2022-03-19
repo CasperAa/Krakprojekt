9f1581fc6b688ae3633529720f7871916647b349
Casper Steengaard <casper.aakviststeengaard@gmail.com>
2022-03-19 22:00:09 +0100

    Added krak files from other project

--- src/test/java/com/example/demokrak2/Demokrak2ApplicationTests.java
+++ src/test/java/com/example/demokrak2/Demokrak2ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.demokrak2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demokrak2ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
